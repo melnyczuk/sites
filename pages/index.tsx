860a088560e1947ac7d09a6dd3cc656f1c47b211
melnyczuk <h.melnyczuk@gmail.com>
2020-04-12 16:13:44 +0100

    💄 add index styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, CSSProperties } from 'react';
 
-const pages = ['imgxxxx'];
+const pages = ['imgxxxx', 'another'];
+
+const styles: Record<HTMLElement['namespaceURI'], CSSProperties> = {
+  a: {
+    fontFamily: '"Archivo", sans-serif',
+    fontSize: '40px',
+    fontWeight: 'bold',
+    padding: '8px',
+    width: '100%',
+    float: 'left',
+  },
+  li: {
+    float: 'left',
+    width: '100%',
+    margin: '2px',
+    borderLeft: '4px solid black',
+    borderTop: '4px solid black',
+  },
+  ul: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '256px',
+  },
+};
 
 const index: FC = () => (
-  <ul>
+  <ul style={styles.ul} >
     {pages.map((page) => (
-      <ul key={page}>
-        <a href={`/${page}`}>{page}</a>
-      </ul>
+      <li style={styles.li} key={page}>
+        <a style={styles.a} href={`/${page}`}>{page}</a>
+      </li>
     ))}
   </ul>
 );
