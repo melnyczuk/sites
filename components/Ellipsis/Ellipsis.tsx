e870b9b4abba00ecb3cf2a30383e0110ee256064
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-04-19 20:02:51 +0100

    💫 add loader (#8)

---
+++ components/Ellipsis/Ellipsis.tsx
@@ -0,0 +1,10 @@
+import React, { FC, useState } from 'react';
+import { useInterval } from 'react-use';
+
+const Ellipsis: FC = () => {
+  const [count, setCount] = useState(0);
+  useInterval(() => { setCount(count + 1); }, 500);
+  return <>{'.'.repeat(count % 4)}</>;
+};
+
+export default Ellipsis;
