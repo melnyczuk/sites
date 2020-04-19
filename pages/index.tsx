2992c63eabe3518af86bbd6f463f0f8403fffd97
melnyczuk <h.melnyczuk@gmail.com>
2020-04-19 22:23:10 +0100

    🍱 add image & deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 import { Styles } from '../types';
 import '../public/static/base.css';
 
-const pages = ['imgxxxx'];
+const pages = ['imgxxxx', 'ahirespngofarock'];
 
 const styles: Styles = {
   a: {
