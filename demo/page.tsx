bcb84ce837759339baea69ca8fcdd806e8643d0d
melnyczuk <h.melnyczuk@gmail.com>
2020-04-19 19:33:15 +0100

    🌱 add new page script

---
+++ demo/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Styles } from '../types';
+import '../public/static/base.css';
+
+const styles: Styles = {};
+
+const page: FC = () => {
+  return <div style={styles}></div>;
+};
+
+export default page;
