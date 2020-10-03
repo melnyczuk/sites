8a3840d288edf21f84d46f6685b882f5377a3dc6
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-10-03 17:40:58 +0100

    ♻️ imgxxxx: use react youtube (#16)
    * lint pages, move legacy to works
    
    * add react-youtube
    
    * mv imgxxxx to works dir, use react-youtube
    
    * update next

---
+++ works/ImgXXXX/ImgXXXX.tsx
@@ -0,0 +1,71 @@
+import React, { FC } from 'react';
+import { useAsyncRetry, useWindowSize } from 'react-use';
+import YouTube from 'react-youtube';
+
+import { VideoInfo } from './types';
+import { VideoDiv, DescDiv, PageDiv } from './styles';
+
+const RATIO = 0.5;
+const ENDPOINT = 'https://imgxxxx.herokuapp.com/info';
+
+const ImgXXXX: FC = () => {
+  const { width: w, height: h } = useWindowSize();
+  const width = w * RATIO;
+  const height = h * RATIO;
+
+  const { loading, error, value, retry } = useAsyncRetry<VideoInfo>(
+    async () => {
+      // eslint-disable-next-line no-undef
+      const r = await fetch(ENDPOINT);
+      return r.json();
+    }
+  );
+
+  if (error) {
+    retry();
+  }
+
+  if (loading) {
+    return (
+      <VideoDiv>
+        <div
+          style={{
+            width,
+            height,
+            background: typeof window === 'undefined' ? 'black' : 'white',
+          }}
+        />
+      </VideoDiv>
+    );
+  }
+
+  const { videoId, title, author, viewCount } = value;
+
+  return (
+    <PageDiv>
+      <VideoDiv>
+        <YouTube
+          videoId={videoId}
+          opts={{
+            width: `${width}`,
+            height: `${height}`,
+            playerVars: { autoplay: 1 },
+          }}
+          onReady={(event): void => {
+            event.target.hideVideoInfo();
+            event.target.playVideo();
+          }}
+          onEnd={retry}
+          onError={retry}
+        />
+      </VideoDiv>
+      <DescDiv>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
+        <p>{viewCount}</p>
+      </DescDiv>
+    </PageDiv>
+  );
+};
+
+export default ImgXXXX;
