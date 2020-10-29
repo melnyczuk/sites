14e51808f47452e630b2f05fb9aed6a34a626753
how melnyczuk <how@melnycz.uk>
2020-10-29 12:02:22 +0000

    add _app to block srr

--- pages/ahirespngofarock.ts
+++ pages/ahirespngofarock.ts
@@ -1,6 +1,1 @@
-import dynamic from 'next/dynamic';
-import Component from '../src/works/ahirespngofarock';
-import '../public/static/base.css';
-
-// eslint-disable-next-line no-undef
-export default dynamic(() => Promise.resolve(Component), { ssr: false });
+export { default } from '../src/works/ahirespngofarock';
